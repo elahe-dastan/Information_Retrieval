49d07f48f881795ceb5eea0c431c2a0513c15369
elahe dastan <elahe.dstn@gmail.com>
2020-12-13 15:43:14 +0330

    merge blocks

--- tokenize/tokenize.go
+++ tokenize/tokenize.go
@@ -1,6 +1,7 @@
 package tokenize
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -15,6 +16,11 @@ import (
 type TermDoc struct {
 	term string // 16 byte
 	doc  int    // 8 byte
+}
+
+type TermPostingList struct {
+	term        string
+	postingList []int // always sorted
 }
 
 var m map[string]int
@@ -75,7 +81,7 @@ func doc(name string) {
 
 		sortedBlock := BlockSort(termDocs)
 		sortedStringInMemory := ""
-		dir := "./blocks/" + strconv.Itoa(block) + ".txt"
+		dir := "./blocks0/" + strconv.Itoa(block) + ".txt"
 		err = os.Chmod(dir, 0777)
 		if err != nil {
 			fmt.Println(err)
@@ -132,7 +138,7 @@ func doc(name string) {
 	}
 
 	//stopWords := stopWord()
-	//merge
+	merge()
 }
 
 func BlockSort(termDocs []TermDoc) []TermDoc {
@@ -179,6 +185,125 @@ func stopWord() []string {
 	return stopWords
 }
 
+func merge() {
+	mergeRun := 0
+	for {
+		// all blocks
+		blocks, err := ioutil.ReadDir("./blocks" + strconv.Itoa(mergeRun))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		blockNames := make([]string, len(blocks))
+
+		for i, b := range blocks {
+			// open each doc and tokenize
+			blockNames[i] = b.Name()
+		}
+
+		// open up to 10 files
+		start := 0
+		size := 10
+		end := start + size
+		filePointers := make([]*bufio.Scanner, size)
+		termPostingLists := make(PairArrayList, size)
+		outputBuffer := make([]byte, 160)
+		newBlocks := 0
+
+		dir := "./blocks" + strconv.Itoa(mergeRun + 1) + "/" + strconv.Itoa(newBlocks) + ".txt"
+		newBlocks++
+		err = os.Chmod(dir, 0777)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		o, err := os.OpenFile(dir, os.O_WRONLY|os.O_CREATE, os.ModeAppend)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		residual := 160
+		for i := start; i < end; i++ {
+			f, err := os.Open("./blocks/" + strconv.Itoa(mergeRun) + blockNames[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			scanner := bufio.NewScanner(f)
+			scanner.Split(bufio.ScanLines)
+			filePointers[i] = scanner
+		}
+
+		for i := 0; i < size; i++ {
+			s := filePointers[i]
+
+			if !s.Scan() {
+				break
+			}
+			l := strings.Split(s.Text(), "")
+			postingList := make([]int, 0)
+			postings := strings.Split(l[1], ",")
+			for j := 0; j < len(postings); j++ {
+				posting, err := strconv.Atoi(postings[j])
+				if err != nil {
+					log.Fatal()
+				}
+
+				postingList = append(postingList, posting)
+			}
+
+			termPostingLists[i] = PairArray{
+				Key:   l[0],
+				Value: postingList,
+			}
+
+		}
+
+		sort.Sort(sort.Reverse(termPostingLists))
+
+		firstTerm := termPostingLists[0].Key
+		firstPostingList := termPostingLists[0].Value
+
+		for i := 1; i < size; i++ {
+			if termPostingLists[i].Key != firstTerm {
+				break
+			}
+
+			for j := 0; i < len(termPostingLists[i].Value); j++ {
+				firstPostingList = append(firstPostingList, termPostingLists[i].Value[j])
+			}
+		}
+
+		sort.Ints(firstPostingList)
+		firstPostingListStr := make([]string, len(firstPostingList))
+		for k := 0; k < len(firstPostingList); k++ {
+			firstPostingListStr = append(firstPostingListStr, strconv.Itoa(firstPostingList[k]))
+		}
+
+		out := firstTerm + " " + strings.Join(firstPostingListStr, ",")
+
+		outBytes := []byte(out)
+
+		if len(outBytes) < residual {
+			outputBuffer = append(outputBuffer, outBytes...)
+			residual -= len(outBytes)
+		} else {
+			outputBuffer = append(outputBuffer, outBytes[:residual]...)
+			_, err := o.Write(outputBuffer)
+			if err != nil {
+				log.Fatal(err)
+			}
+			outBytes = outBytes[residual:]
+		}
+
+		_, err = o.Write(outputBuffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		mergeRun++
+	}
+}
+
 type Pair struct {
 	Key   string
 	Value int
@@ -187,5 +312,16 @@ type Pair struct {
 type PairList []Pair
 
 func (p PairList) Len() int           { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Less(i, j int) bool { return p[i].Key < p[j].Key }
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+type PairArray struct {
+	Key   string
+	Value []int
+}
+
+type PairArrayList []PairArray
+
+func (p PairArrayList) Len() int           { return len(p) }
+func (p PairArrayList) Less(i, j int) bool { return p[i].Key < p[j].Key }
+func (p PairArrayList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
