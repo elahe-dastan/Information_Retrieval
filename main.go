26725e9e1dff1daf05dfe0fe340b760ab8de074d
Elahe Dastan <elahe.dstn@gmail.com>
2021-01-04 17:13:45 +0330

    add docs

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import champion_list "Information_Retrieval/champion-list"
 
 func main() {
-	//i := index.NewIndex("./docs", 6)
+	//i := index.NewIndex("./p_docs", 6)
 	//indexFile := i.Construct()
 	//v := vector_space.NewVectorizer(indexFile, 3)
 	//v.Vectorize()
